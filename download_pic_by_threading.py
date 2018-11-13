a28fad0d0542303bf0fd68823285bf5dd7af7944
kongp <kongpahuixiao@sina.com>
2018-11-13 14:54:29 +0800

    添加pic_log(),write_url()

--- download_pic_by_threading.py
+++ download_pic_by_threading.py
@@ -9,18 +9,19 @@ __desc__ = 通过threading方式进行多进程下载,这个比较熟悉
 
 import threading
 import datetime
-import os
 import queue
+import os
 import requests
 
-from get_config import GetConfig
+
 from pic_file_handle import PicFileHandle
 from random_sleep_time import RandomSleepTime
 from get_title_urls import GetTitleUrls
 from get_pic_url_in_pages import GetPicUrlInPages
+from logging_info import console_log
 
 NOW_DATE = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
-NOW_TIME = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
+NOW_TIME = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_%H%M%S')
 
 class DownloadPicByThreading(threading.Thread):
     """多线程下载
@@ -32,11 +33,13 @@ class DownloadPicByThreading(threading.Thread):
     
     """
 
-    def __init__(self, que_):
+    def __init__(self, que_, f_urls):
         threading.Thread.__init__(self)
 
         self.sleep_program = RandomSleepTime()
         self.que = que_
+        self.f_urls = f_urls
+        # print(self.f_urls.closed)
     
     def run(self):
         while True:
@@ -46,16 +49,44 @@ class DownloadPicByThreading(threading.Thread):
             img_contents = requests.get(url).content
             with open(pic_file_path, 'wb') as f:
                 f.write(img_contents)
+                self.pic_log(url, os.path.split(pic_file_path)[1])
             
+            self.write_url(url)
             self.que.task_done()
 
+    def pic_log(self, url, pic_name):
+        """插入日志
+        
+        Parameters
+        ----------
+        url : str
+            pic网址
+        pic_name : str
+            pic名称
+        
+        """
+
+        log_path = PicFileHandle.get_logger_file_path()
+        console_log(url, pic_name, log_path)
+
+    def write_url(self, url):
+        """将已下载的图片网址写入downloaded_urls.txt
+        
+        Parameters
+        ----------
+        url : str
+            pic网址
+        
+        """
+
+        self.f_urls.write(url + '\n')
 
 if __name__ == '__main__':
-    """
-    1.获取所有的title
-    2.解析title,获取title标题(pic_title),title中的说明(pic_explain),title中所有pic网址
-    3.根据pic_title创建存储的文件夹
-    """
+
+    # 1.获取所有的title
+    # 2.解析title,获取title标题(pic_title),title中的说明(pic_explain),title中所有pic网址
+    # 3.根据pic_title创建存储的文件夹
+
     que = queue.Queue()
 
     title_urls = GetTitleUrls().start()
@@ -70,21 +101,24 @@ if __name__ == '__main__':
         pic_folder_path = PicFileHandle.get_pic_folder_path(pic_title)
         # 创建存储pic的文件夹
         PicFileHandle.create_folder(pic_folder_path)
+        # 获取downloaded_urls文件的路径
+        downloaded_urls_path = PicFileHandle.get_downloaded_urls_path()
 
         for k in pic_info:
             if k not in ('pic_explain', 'pic_title'):
                 mid_pic_file_path = PicFileHandle.get_pic_file_path(k, pic_info[k], pic_folder_path)
                 que.put((k, mid_pic_file_path))
         
-        for _ in range(5):
-            t = DownloadPicByThreading(que)
-            t.setDaemon(True)
-            t.start()
+        with open(downloaded_urls_path, 'a', encoding='utf-8') as f_url:
+            for _ in range(5):
+                t = DownloadPicByThreading(que, f_url)
+                t.setDaemon(True)
+                t.start()
 
-        que.join()
+            que.join()
 
 
-'''为什么不使用 t = DownloadPicByThreading(que, pic_info) 中pic_info(dict)?
-因为在多线程中,线程A中可能是已经传入了新的页面的网址,线程B仍就使用pic_info,导致数据差异
-产生keyError,所以将其拆分,直接传入数据
-'''
+
+# 为什么不使用 t = DownloadPicByThreading(que, pic_info) 中pic_info(dict)?
+# 因为在多线程中,线程A中可能是已经传入了新的页面的网址,线程B仍就使用pic_info,导致数据差异
+# 产生keyError,所以将其拆分,直接传入数据
