1a1432386b068a9c0b26a36317cb77368917258c
kongp <kongpahuixiao@sina.com>
2018-11-18 18:12:39 +0800

    添加一个IOError的处理

--- download_pic_by_threading.py
+++ download_pic_by_threading.py
@@ -41,6 +41,8 @@ class DownloadPicByThreading(threading.Thread):
         self.downloaded_urls_path = PicFileHandle.get_downloaded_urls_path()
         self.log_path = log_path
         self.f_write_urls = f_write_urls
+        # 异常处理
+        self.exc = None
 
     def run(self):
         while True:
@@ -54,14 +56,22 @@ class DownloadPicByThreading(threading.Thread):
             mid_pic_name = mid_pic_name + '.' + content_type.split('/')[1]
             pic_file_path = PicFileHandle.path_join(mid_pic_folder_path, mid_pic_name)
 
-            with open(pic_file_path, 'wb') as f_pics:
-                f_pics.write(img_contents)
-                self.f_write_urls.write(url + '\n')
-                runing_time = datetime.datetime.now() - start_time
-                runing_time = str(runing_time.seconds) + '.' + str(runing_time.microseconds)[:2]  
-                self.pic_log(runing_time, os.path.split(pic_file_path)[1], url)
-            
-            self.que.task_done()
+            # 添加子线程的异常处理
+            try:
+                with open(pic_file_path, 'wb') as f_pics:
+                    f_pics.write(img_contents)
+                    self.f_write_urls.write(url + '\n')
+                    runing_time = datetime.datetime.now() - start_time
+                    runing_time = str(runing_time.seconds) + '.' + str(runing_time.microseconds)[:2]  
+                    self.pic_log(runing_time, os.path.split(pic_file_path)[1], url)
+
+            except IOError as io_e:
+                import sys
+                print('Exception e is ' + str(io_e))
+                self.exc = sys.exc_info()
+
+            finally:
+                self.que.task_done()
 
     def pic_log(self, download_time, pic_name, url):
         """插入日志
@@ -97,26 +107,47 @@ if __name__ == '__main__':
     # 获取downloaded_urls内容
     downloaded_urls = PicFileHandle.get_downloaded_urls()
 
-    for title_url in title_urls:
-        pic_info = GetPicInfoInTitlePages().return_pic_info(title_url, if_break=True)
+    pic_info = {'pic_explain': '9月17日是日本的《敬老节》，近日人气急升的漫画《黄金神威》则适时推出敬老版《白银神威》，原作中诸位豪杰全部化为白发老人，读起来别有一番滋味。', 'pic_title': '日式搞笑来袭！人气漫画《黄金神威》推敬老白银神威', 'https://img.3dmgame.com/uploads/images/news/20180918/1537262474_106428.png': '0101_·一起来欣赏下对比后的画面，怎样，突然老化后的众位角色是不是另有一番新感觉？感兴趣的小伙伴可以去特设网站阅读。https://youngjump.jp/goldenkamuy/contents/silverkamuy/', 'https://img.3dmgame.com/uploads/images/news/20180918/1537262486_229261.png': '0102_None_20181118_180307_1', 'https://img.3dmgame.com/uploads/images/news/20180918/1537262486_888511.png': '0103_None_20181118_180307_2', 'https://img.3dmgame.com/uploads/images/news/20180918/1537262486_163710.png': '0104_None_20181118_180307_3', 'https://img.3dmgame.com/uploads/images/news/20180918/1537262486_732005.png': '0105_None_20181118_180307_4'}
 
-        pic_explain = pic_info.get('pic_explain', 'None_Pic_Explain_' + NOW_TIME)
-        pic_title = pic_info.get('pic_title', 'None_Pic_Title_' + NOW_TIME)
+    pic_explain = pic_info.get('pic_explain', 'None_Pic_Explain_' + NOW_TIME)
+    pic_title = pic_info.get('pic_title', 'None_Pic_Title_' + NOW_TIME)
 
-        # 获取存储pic的文件夹路径
-        pic_folder_path = PicFileHandle.get_pic_folder_path(pic_title)
-        # 创建存储pic的文件夹
-        PicFileHandle.create_folder(pic_folder_path)
-        # 将pic_explain写入到pic文件夹内
-        PicFileHandle.write_pic_explain(title_url, pic_explain, pic_folder_path)
+    # 获取存储pic的文件夹路径
+    pic_folder_path = PicFileHandle.get_pic_folder_path(pic_title)
+    # 创建存储pic的文件夹
+    PicFileHandle.create_folder(pic_folder_path)
+    # 将pic_explain写入到pic文件夹内
+    title_url = 'https://www.3dmgame.com/bagua_65_1/'
+    PicFileHandle.write_pic_explain(title_url, pic_explain, pic_folder_path)
 
-        for k in pic_info:
-            if k not in ('pic_explain', 'pic_title') and k not in downloaded_urls:
-                # mid_pic_file_path = PicFileHandle.get_pic_file_path(
-                #     k, pic_info[k], pic_folder_path)
-                que.put((k, pic_info[k], pic_folder_path))
+    for k in pic_info:
+        if k not in ('pic_explain', 'pic_title') and k not in downloaded_urls:
+            # mid_pic_file_path = PicFileHandle.get_pic_file_path(
+            #     k, pic_info[k], pic_folder_path)
+            que.put((k, pic_info[k], pic_folder_path))
 
-                break
+            break
+            
+    # for title_url in title_urls:
+    #     pic_info = GetPicInfoInTitlePages().return_pic_info(title_url, if_break=True)
+
+    #     pic_explain = pic_info.get('pic_explain', 'None_Pic_Explain_' + NOW_TIME)
+    #     pic_title = pic_info.get('pic_title', 'None_Pic_Title_' + NOW_TIME)
+
+    #     # 获取存储pic的文件夹路径
+    #     pic_folder_path = PicFileHandle.get_pic_folder_path(pic_title)
+    #     # 创建存储pic的文件夹
+    #     PicFileHandle.create_folder(pic_folder_path)
+    #     # 将pic_explain写入到pic文件夹内
+    #     PicFileHandle.write_pic_explain(title_url, pic_explain, pic_folder_path)
+
+    #     for k in pic_info:
+    #         if k not in ('pic_explain', 'pic_title') and k not in downloaded_urls:
+    #             # mid_pic_file_path = PicFileHandle.get_pic_file_path(
+    #             #     k, pic_info[k], pic_folder_path)
+    #             que.put((k, pic_info[k], pic_folder_path))
+
+    #             break
     
     # 关于这个循环放在for k in pic_info外面的解释
     # 当这个循环在里面时,每个循环,都会创建五个线程,造成线程超多
